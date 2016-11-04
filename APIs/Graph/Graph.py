d38c0b9e9e3a7ec5a49b4f49eaa0d2304b99f34d
Vaibhav Sharma <vaibhav.s.sharma@outlook.com>
2016-11-03 22:16:58 -0500

    Added Docstrings

--- APIs/Graph/Graph.py
+++ APIs/Graph/Graph.py
@@ -7,22 +7,39 @@ Created on Thu Nov  3 20:09:21 2016
 # GRAPH API
 
 class GraphNode(object):
+    """
+        Utility Class for saving data    
+    """
     def __init__(self,key,value=None):
         self.key = key
         self.value = value
 
 class Graph(object):
+    """
+        Graph API
+    """    
+    
     def __init__(self,vertices=None,diGraph=False):
+        """
+            Creates a Graph/DiGraph
+        """        
+
         self.Graph = {}
         self.diGraph = diGraph
         if vertices:
             for v in vertices:
                 self.Graph[v] = set()
     def addVertex(self,v):
+        """
+            Add Vertex
+        """   
         if v not in self.Graph:
             self.Graph[v] = set()
             
     def addEdge(self,v1,v2):
+        """
+            Add Edge
+        """        
         if v1 not in self.Graph:
             raise ValueError("{} not in graph".format(v1))
         if v2 not in self.Graph:
@@ -32,6 +49,9 @@ class Graph(object):
         if not self.diGraph:
              self.Graph[v2].add(v1)   
     def prettyPrint(self):
+        """
+            Pretty Print the Graph
+        """        
         for vertex in self.Graph:
             adjLst = self.Graph[vertex]
             print "{} -> ".format(vertex), 
@@ -39,6 +59,11 @@ class Graph(object):
                 print "{} ".format(item),
             print 
     def connectedComponents(self):
+        """
+            Counts Connected Components
+            Returns count and dictionary representing to which connected 
+            component specific vertex lies
+        """        
         marked = {}
         belongsTo = {}
         for key in self.Graph:
@@ -52,6 +77,9 @@ class Graph(object):
         return count,belongsTo
 
     def __dfsCC(self,vertex,marked,count,belongsTo):
+        """
+            Private method for connected components
+        """        
         marked[vertex] = True
         belongsTo[vertex] = count
         for v in self.Graph[vertex]:
